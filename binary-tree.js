f9c7abb8d58bb1b524b11abce7060c5a6a0bfffe
nsasinovich <ns.univer2@gmail.com>
2016-01-29 17:57:12 +0300

    added insert function

--- binary-tree.js
+++ binary-tree.js
@@ -2,11 +2,42 @@
 
 class BinaryTree {
 	constructor() {
-
+		this.root = null;
 	}
 
 	insert(data) {
+		//create new Node item and fill it by input data
+		var node = new Node(data);
+		var current;
 
+		//special case: the tree is empty yet
+		if (this.root === null) {
+			this.root = node;
+		} else {
+			current = this.root;
+
+			//add new item to a tree according to binary tree property
+			while(true) {
+				if (data < current.data) {
+					if (current.left === null) {
+						current.left = node;
+						break;
+					} else {
+						current = current.left;
+					}
+
+
+				} else if (data > current.data) {
+					if (current.right === null) {
+						current.right = node;
+						break;
+					} else {
+						current = current.right;
+					}
+
+				} else break;
+			}
+		}
 	}
 
 	contains(data) {
